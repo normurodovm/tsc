74f2627ac1c3044437064f6fe2b7030fe6fee6c1
normurodovm <nmaqsud302@gmail.com>
2024-08-23 15:22:41 +0500

    message

---
+++ main.ts
@@ -0,0 +1,546 @@
+//                  1-masala
+// const num = Number(prompt("son kiriting"))
+// function isPrime(num: number): boolean {
+//     if (num <= 1) return false;
+//     if (num === 2) return true;
+//     if (num % 2 === 0) return false;
+
+//     for (let i = 3; i <= Math.sqrt(num); i += 2) {
+//         if (num % i === 0) return false;
+//     }
+    
+//     return true;
+// }
+// if (isPrime(num)) {
+//     alert("son tub")
+// }else{
+//     alert("son tub emas")
+// }
+
+
+
+
+
+
+
+//                  2-masala
+// interface UserType  {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+//   address: {
+//     street: string;
+//     suite: string;
+//     city: string;
+//     zipcode: string;
+//     geo: {
+//       lat: string;
+//       lng: string;
+//     };
+//   };
+//   phone: string;
+//   website: string;
+//   company: {
+//     name: string;
+//     catchPhrase: string;
+//     bs: string;
+//   };
+// };
+
+// const arr: UserType[] = [
+//   {
+//     id: 1,
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: {
+//       street: "Kulas Light",
+//       suite: "Apt. 556",
+//       city: "Gwenborough",
+//       zipcode: "92998-3874",
+//       geo: {
+//         lat: "-37.3159",
+//         lng: "81.1496",
+//       },
+//     },
+//     phone: "1-770-736-8031 x56442",
+//     website: "hildegard.org",
+//     company: {
+//       name: "Romaguera-Crona",
+//       catchPhrase: "Multi-layered client-server neural-net",
+//       bs: "harness real-time e-markets",
+//     },
+//   },
+//   {
+//     id: 2,
+//     name: "Ervin Howell",
+//     username: "Antonette",
+//     email: "Shanna@melissa.tv",
+//     address: {
+//       street: "Victor Plains",
+//       suite: "Suite 879",
+//       city: "Wisokyburgh",
+//       zipcode: "90566-7771",
+//       geo: {
+//         lat: "-43.9509",
+//         lng: "-34.4618",
+//       },
+//     },
+//     phone: "010-692-6593 x09125",
+//     website: "anastasia.net",
+//     company: {
+//       name: "Deckow-Crist",
+//       catchPhrase: "Proactive didactic contingency",
+//       bs: "synergize scalable supply-chains",
+//     },
+//   },
+//   {
+//     id: 3,
+//     name: "Clementine Bauch",
+//     username: "Samantha",
+//     email: "Nathan@yesenia.net",
+//     address: {
+//       street: "Douglas Extension",
+//       suite: "Suite 847",
+//       city: "McKenziehaven",
+//       zipcode: "59590-4157",
+//       geo: {
+//         lat: "-68.6102",
+//         lng: "-47.0653",
+//       },
+//     },
+//     phone: "1-463-123-4447",
+//     website: "ramiro.info",
+//     company: {
+//       name: "Romaguera-Jacobson",
+//       catchPhrase: "Face to face bifurcated interface",
+//       bs: "e-enable strategic applications",
+//     },
+//   },
+//   {
+//     id: 4,
+//     name: "Patricia Lebsack",
+//     username: "Karianne",
+//     email: "Julianne.OConner@kory.org",
+//     address: {
+//       street: "Hoeger Mall",
+//       suite: "Apt. 692",
+//       city: "South Elvis",
+//       zipcode: "53919-4257",
+//       geo: {
+//         lat: "29.4572",
+//         lng: "-164.2990",
+//       },
+//     },
+//     phone: "493-170-9623 x156",
+//     website: "kale.biz",
+//     company: {
+//       name: "Robel-Corkery",
+//       catchPhrase: "Multi-tiered zero tolerance productivity",
+//       bs: "transition cutting-edge web services",
+//     },
+//   },
+//   {
+//     id: 5,
+//     name: "Chelsey Dietrich",
+//     username: "Kamren",
+//     email: "Lucio_Hettinger@annie.ca",
+//     address: {
+//       street: "Skiles Walks",
+//       suite: "Suite 351",
+//       city: "Roscoeview",
+//       zipcode: "33263",
+//       geo: {
+//         lat: "-31.8129",
+//         lng: "62.5342",
+//       },
+//     },
+//     phone: "(254)954-1289",
+//     website: "demarco.info",
+//     company: {
+//       name: "Keebler LLC",
+//       catchPhrase: "User-centric fault-tolerant solution",
+//       bs: "revolutionize end-to-end systems",
+//     },
+//   },
+//   {
+//     id: 6,
+//     name: "Mrs. Dennis Schulist",
+//     username: "Leopoldo_Corkery",
+//     email: "Karley_Dach@jasper.info",
+//     address: {
+//       street: "Norberto Crossing",
+//       suite: "Apt. 950",
+//       city: "South Christy",
+//       zipcode: "23505-1337",
+//       geo: {
+//         lat: "-71.4197",
+//         lng: "71.7478",
+//       },
+//     },
+//     phone: "1-477-935-8478 x6430",
+//     website: "ola.org",
+//     company: {
+//       name: "Considine-Lockman",
+//       catchPhrase: "Synchronised bottom-line interface",
+//       bs: "e-enable innovative applications",
+//     },
+//   },
+//   {
+//     id: 7,
+//     name: "Kurtis Weissnat",
+//     username: "Elwyn.Skiles",
+//     email: "Telly.Hoeger@billy.biz",
+//     address: {
+//       street: "Rex Trail",
+//       suite: "Suite 280",
+//       city: "Howemouth",
+//       zipcode: "58804-1099",
+//       geo: {
+//         lat: "24.8918",
+//         lng: "21.8984",
+//       },
+//     },
+//     phone: "210.067.6132",
+//     website: "elvis.io",
+//     company: {
+//       name: "Johns Group",
+//       catchPhrase: "Configurable multimedia task-force",
+//       bs: "generate enterprise e-tailers",
+//     },
+//   },
+// ];
+
+// function isPrime(num: number): boolean {
+//     if (num <= 1) return false;
+//     if (num === 2) return true;
+//     if (num % 2 === 0) return false;
+
+//     for (let i = 3; i <= Math.sqrt(num); i += 2) {
+//         if (num % i === 0) return false;
+//     }
+
+//     return true;
+// }
+
+// const newarr: UserType[] = arr.filter(user => isPrime(user.id));
+
+// console.log(newarr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//              3-masala
+// interface User {
+//     id: number;
+//     name: string;
+//     salaries: number[];
+//     totalSalary?: number;
+// }
+
+// function calculateTotalSalary(user: User): User {
+//     const totalSalary = user.salaries.reduce((acc, salary) => acc + salary, 0);
+//     return { ...user, totalSalary };
+// }
+
+// const user: User = {
+//     id: 1,
+//     name: "John Doe",
+//     salaries: [1500, 1600, 1700],
+// };
+
+// const updatedUser = calculateTotalSalary(user);
+
+// console.log(updatedUser);
+
+
+
+
+
+
+
+
+
+
+//          4-masala
+// function getUniqueNumbers(numbers: number[]): number[] {
+//     return Array.from(new Set(numbers));
+// }
+
+// const numbers = [1, 2, 2, 3, 4, 4, 5, 6, 6];
+
+// const uniqueNumbers = getUniqueNumbers(numbers);
+
+// console.log(uniqueNumbers);
+
+
+
+
+
+
+
+
+
+//            5-masala
+// interface User {
+//     id: number;
+//     name: string;
+// }
+
+// function removeDuplicateUsers(users: User[]): User[] {
+//     const uniqueUsersMap = new Map<number, User>();
+
+//     users.forEach(user => {
+//         uniqueUsersMap.set(user.id, user);
+//     });
+
+//     return Array.from(uniqueUsersMap.values());
+// }
+
+// const users: User[] = [
+//     { id: 1, name: "John Doe" },
+//     { id: 2, name: "Jane Smith" },
+//     { id: 3, name: "Alice Johnson" },
+//     { id: 2, name: "Bob Brown" },  
+//     { id: 4, name: "Charlie Davis" },
+//     { id: 1, name: "Eva Green" }   
+// ];
+
+// const uniqueUsers = removeDuplicateUsers(users);
+
+// console.log(uniqueUsers);
+
+
+
+
+
+//        QO'SHIMCHA
+
+//toq sonni aniqlash
+// let count:number = Number(prompt("raqam kiriting"))
+
+
+// const x = (count:number):boolean => {
+//     return count%2!==0
+// }
+// console.log(x(count));
+
+
+
+//       IDsi juft bo'lganlarini yangi arrayga solib qaytaradi   
+// interface UserType  {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+//   address: {
+//     street: string;
+//     suite: string;
+//     city: string;
+//     zipcode: string;
+//     geo: {
+//       lat: string;
+//       lng: string;
+//     };
+//   };
+//   phone: string;
+//   website: string;
+//   company: {
+//     name: string;
+//     catchPhrase: string;
+//     bs: string;
+//   };
+// };
+
+// const arr: UserType[] = [
+//   {
+//     id: 1,
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: {
+//       street: "Kulas Light",
+//       suite: "Apt. 556",
+//       city: "Gwenborough",
+//       zipcode: "92998-3874",
+//       geo: {
+//         lat: "-37.3159",
+//         lng: "81.1496",
+//       },
+//     },
+//     phone: "1-770-736-8031 x56442",
+//     website: "hildegard.org",
+//     company: {
+//       name: "Romaguera-Crona",
+//       catchPhrase: "Multi-layered client-server neural-net",
+//       bs: "harness real-time e-markets",
+//     },
+//   },
+//   {
+//     id: 2,
+//     name: "Ervin Howell",
+//     username: "Antonette",
+//     email: "Shanna@melissa.tv",
+//     address: {
+//       street: "Victor Plains",
+//       suite: "Suite 879",
+//       city: "Wisokyburgh",
+//       zipcode: "90566-7771",
+//       geo: {
+//         lat: "-43.9509",
+//         lng: "-34.4618",
+//       },
+//     },
+//     phone: "010-692-6593 x09125",
+//     website: "anastasia.net",
+//     company: {
+//       name: "Deckow-Crist",
+//       catchPhrase: "Proactive didactic contingency",
+//       bs: "synergize scalable supply-chains",
+//     },
+//   },
+//   {
+//     id: 3,
+//     name: "Clementine Bauch",
+//     username: "Samantha",
+//     email: "Nathan@yesenia.net",
+//     address: {
+//       street: "Douglas Extension",
+//       suite: "Suite 847",
+//       city: "McKenziehaven",
+//       zipcode: "59590-4157",
+//       geo: {
+//         lat: "-68.6102",
+//         lng: "-47.0653",
+//       },
+//     },
+//     phone: "1-463-123-4447",
+//     website: "ramiro.info",
+//     company: {
+//       name: "Romaguera-Jacobson",
+//       catchPhrase: "Face to face bifurcated interface",
+//       bs: "e-enable strategic applications",
+//     },
+//   },
+//   {
+//     id: 4,
+//     name: "Patricia Lebsack",
+//     username: "Karianne",
+//     email: "Julianne.OConner@kory.org",
+//     address: {
+//       street: "Hoeger Mall",
+//       suite: "Apt. 692",
+//       city: "South Elvis",
+//       zipcode: "53919-4257",
+//       geo: {
+//         lat: "29.4572",
+//         lng: "-164.2990",
+//       },
+//     },
+//     phone: "493-170-9623 x156",
+//     website: "kale.biz",
+//     company: {
+//       name: "Robel-Corkery",
+//       catchPhrase: "Multi-tiered zero tolerance productivity",
+//       bs: "transition cutting-edge web services",
+//     },
+//   },
+//   {
+//     id: 5,
+//     name: "Chelsey Dietrich",
+//     username: "Kamren",
+//     email: "Lucio_Hettinger@annie.ca",
+//     address: {
+//       street: "Skiles Walks",
+//       suite: "Suite 351",
+//       city: "Roscoeview",
+//       zipcode: "33263",
+//       geo: {
+//         lat: "-31.8129",
+//         lng: "62.5342",
+//       },
+//     },
+//     phone: "(254)954-1289",
+//     website: "demarco.info",
+//     company: {
+//       name: "Keebler LLC",
+//       catchPhrase: "User-centric fault-tolerant solution",
+//       bs: "revolutionize end-to-end systems",
+//     },
+//   },
+//   {
+//     id: 6,
+//     name: "Mrs. Dennis Schulist",
+//     username: "Leopoldo_Corkery",
+//     email: "Karley_Dach@jasper.info",
+//     address: {
+//       street: "Norberto Crossing",
+//       suite: "Apt. 950",
+//       city: "South Christy",
+//       zipcode: "23505-1337",
+//       geo: {
+//         lat: "-71.4197",
+//         lng: "71.7478",
+//       },
+//     },
+//     phone: "1-477-935-8478 x6430",
+//     website: "ola.org",
+//     company: {
+//       name: "Considine-Lockman",
+//       catchPhrase: "Synchronised bottom-line interface",
+//       bs: "e-enable innovative applications",
+//     },
+//   },
+//   {
+//     id: 7,
+//     name: "Kurtis Weissnat",
+//     username: "Elwyn.Skiles",
+//     email: "Telly.Hoeger@billy.biz",
+//     address: {
+//       street: "Rex Trail",
+//       suite: "Suite 280",
+//       city: "Howemouth",
+//       zipcode: "58804-1099",
+//       geo: {
+//         lat: "24.8918",
+//         lng: "21.8984",
+//       },
+//     },
+//     phone: "210.067.6132",
+//     website: "elvis.io",
+//     company: {
+//       name: "Johns Group",
+//       catchPhrase: "Configurable multimedia task-force",
+//       bs: "generate enterprise e-tailers",
+//     },
+//   },
+// ];
+
+// const newarr:UserType[] = [
+
+// ]
+
+//     for(let i of arr ){
+//         if(i.id%2==0){
+//             newarr.push(i) 
+//         }
+        
+//     }
+    
+//     console.log(newarr);
+
+
+    
